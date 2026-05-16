860ed131d3b6939c7e0c38598f8ca868044fabea
Ntope1983 <g_poly7@yahoo.gr>
2026-05-16 22:02:50 +0300

    db inserted v.2

---
+++ Order.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+public class Order
+{
+    public int Id { get; set; }
+    [Required]
+    public User OrderUser { get; set; }
+    [Required]
+    public List<Game> OrderGames { get; set; }
+    [Required]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+}
---
+++ User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+public class User
+{
+    
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(15)]
+    public string Username { get; set; } = string.Empty;
+    [Required]
+    public string Email { get; set; } = string.Empty;
+}
